0356b1d0c2e9142bdafd597feafeaaf51d4afccf
Sushant <sushantkumar6700@gmail.com>
2021-09-11 23:21:46 +0530

    added frontent files and requirements txt

--- craigslist/myapp/views.py
+++ craigslist/myapp/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    frontend = {
+        'search': search,
+    }
+    return render(request, 'myapp/new_search.html', frontend)
